cc4d47d26c8290bc18f356ac09a30fdc4b0788d5
Kennedy Wambua <kennywambu254@gmail.com>
2022-09-05 01:39:16 +0300

    adding swahili language

---
+++ src/components/Translate.js
@@ -0,0 +1,52 @@
+
+
+import React from "react";
+import Dropdown from "./Dropdown";
+import Convert from "./Convert";
+
+const options = [
+    {
+        label: 'Afrikaans',
+        value: 'af'
+    },
+    {
+        label: 'Arabic',
+        value: 'ar'
+    },
+    {
+        label: 'Hindi',
+        value: 'hi'
+    },
+    {
+        label:'Swahili',
+        value:'sw'
+    }
+]
+
+const Translate = () => {
+    //states
+    const [language, setLanguage] = React.useState(options[0])
+    const [text, setText] = React.useState('')
+
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Text</label>
+                    <input value={text} onChange={(e) => setText(e.target.value)} />
+                </div>
+            </div>
+            <Dropdown
+            label ='select a language'
+            selected={language}
+            onSelectedChange={setLanguage}
+            options={options} />
+            <hr/>
+            <h3 className="ui header">Output</h3>
+            <Convert text={text} language={language} />
+        </div>
+    )
+
+}
+
+export default Translate

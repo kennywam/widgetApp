36db6b44530dbe55c873e195db55549a2de18542
Kennedy Wambua <kennywambu254@gmail.com>
2022-09-06 16:34:42 +0300

    adding translation languages

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,52 +1,54 @@
-
-
 import React from "react";
 import Dropdown from "./Dropdown";
 import Convert from "./Convert";
 
 const options = [
-    {
-        label: 'Afrikaans',
-        value: 'af'
-    },
-    {
-        label: 'Arabic',
-        value: 'ar'
-    },
-    {
-        label: 'Hindi',
-        value: 'hi'
-    },
-    {
-        label:'Swahili',
-        value:'sw'
-    }
-]
+  {
+    label: "Afrikaans",
+    value: "af",
+  },
+  {
+    label: "Arabic",
+    value: "ar",
+  },
+  {
+    label: "Hindi",
+    value: "hi",
+  },
+  {
+    label: "Swahili",
+    value: "sw",
+  },
+  {
+    label: "English",
+    value: "en",
+  },
+];
 
 const Translate = () => {
-    //states
-    const [language, setLanguage] = React.useState(options[0])
-    const [text, setText] = React.useState('')
+  //states
+  const [language, setLanguage] = React.useState(options[0]);
+  const [text, setText] = React.useState("");
 
-    return (
-        <div>
-            <div className="ui form">
-                <div className="field">
-                    <label>Enter Text</label>
-                    <input value={text} onChange={(e) => setText(e.target.value)} />
-                </div>
-            </div>
-            <Dropdown
-            label ='select a language'
-            selected={language}
-            onSelectedChange={setLanguage}
-            options={options} />
-            <hr/>
-            <h3 className="ui header">Output</h3>
-            <Convert text={text} language={language} />
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Text</label>
+          <input value={text} onChange={(e) => setText(e.target.value)} />
         </div>
-    )
+      </div>
+      <Dropdown
+        label="select a language"
+        selected={language}
+        onSelectedChange={setLanguage}
+        options={options}
+      />
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert text={text} language={language} />
+    </div>
+  );
+};
 
-}
-
-export default Translate
+export default Translate;

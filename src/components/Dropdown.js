7d2e986e2745101608fcd3204264d8f169a0310e
Kennedy Wambua <kennywambu254@gmail.com>
2022-09-06 16:33:47 +0300

    rendered options update

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,69 +1,69 @@
 import React from "react";
 
-const Dropdown = ({label, options, selected, onSelectedChange}) => {
-    // state
-    const [open, setOpen] = React.useState(false)
-    //ref
-    const ref = React.useRef()
+const Dropdown = ({ label, options, selected, onSelectedChange }) => {
+  // state
+  const [open, setOpen] = React.useState(false);
+  //ref
+  const ref = React.useRef();
 
-    // methods
-    const renderedOptions = options.map((option) => {
-        if (option.value === selected.value){
-            return null//this prevents the slected color from appearing on 
-                        //the list
-        }
-        return(
-            <div 
-                onClick = {() => onSelectedChange(option)}//
-                key={option.value} 
-                className="item"
-            >
-                {option.label}
-            </div>
-        )
-    })
-
-
-    const handleClick = (event) => {//Handles dropdown when whit space is clicked 
-
-        if (ref.current.contains(event.target)){
-            return
-        }
-            setOpen(false) 
+  // methods
+  const renderedOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      return null; //this prevents the slected option from appearing on
+      //the list
     }
-
-    React.useEffect(() => {
-        document.addEventListener("click", handleClick)
-
-        return () => {
-            document.removeEventListener("click", handleClick)//clean up  fuction
-        }
-    }, [])
-
-    const onHandleClick = (event) =>{
-        event.stopPropagation()
-        setOpen(!open)
-    }
-        
     return (
-        <div ref={ref} className="ui form">
-            <div className="field">
-                <label className="label">{label}</label>
-                <div   
-                    onClick={onHandleClick}
-                    className={`ui selection dropdown ${open ? 'visible active' : ''}`}
-                   
-                    
-                >
-                    <i className="dropdown icon"></i>
-                    <div className="text" style={{color:selected.value}}>{selected.label}</div>
-                    <div className={`menu ${open ? 'visible transition' : ''}`}>
-                        {renderedOptions}
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+      <div
+        onClick={() => onSelectedChange(option)} //
+        key={option.value}
+        className="item"
+      >
+        {option.label}
+      </div>
+    );
+  });
 
-export default Dropdown
+  const handleClick = (event) => {
+    //Handles dropdown when whit space is clicked
+
+    if (ref.current.contains(event.target)) {
+      return;
+    }
+    setOpen(false);
+  };
+
+  React.useEffect(() => {
+    document.addEventListener("click", handleClick);
+
+    return () => {
+      document.removeEventListener("click", handleClick); //clean up  fuction
+    };
+  }, []);
+
+  const onHandleClick = (event) => {
+    event.stopPropagation();
+    setOpen(!open);
+  };
+
+  return (
+    <div ref={ref} className="ui form">
+      <div className="field">
+        <label className="label">{label}</label>
+        <div
+          onClick={onHandleClick}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
+          <i className="dropdown icon"></i>
+          <div className="text" style={{ color: selected.value }}>
+            {selected.label}
+          </div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;

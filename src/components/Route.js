4e7945556d97f04dbf88bb06b384e98d15378efc
Kennedy Wambua <kennywambu254@gmail.com>
2022-09-06 18:25:22 +0300

    adding eventListener to rerender Route

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,5 +1,22 @@
+import React from "react";
+
 const Route = ({ path, children }) => {
-  return window.location.pathname === path ? children : null;
+  //states
+  const [currentPath, setCurrentPath] = React.useState(
+    window.location.pathname
+  ); //To cause Route
+  React.useEffect(() => {
+    //to rerender
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname);
+    };
+    window.addEventListener("popstate", onLocationChange); //listens to change of popstate
+
+    return () => {
+      window.removeEventListener("popstate", onLocationChange);
+    };
+  }, []);
+  return currentPath === path ? children : null;
 };
 
 export default Route;

d624809a151c570e4aeabbb617cbdde240aa88b0
Kennedy Wambua <kennywambu254@gmail.com>
2022-09-06 16:28:09 +0300

    adding navigations

--- src/App.js
+++ src/App.js
@@ -5,63 +5,74 @@ import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
 
 const items = [
-    {
-        title: 'What is React?',
-        content: "React is a front end Javascript framework"
-    },
-    {
-        title: "Why use React?",
-        content: "Reac is a good Js Library among Engineers"
-    },
-    {
-        title: "How do you use React?",
-        content: "You use React by creating components"
-    }
-]
+  {
+    title: "What is React?",
+    content: "React is a front end Javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "Reac is a good Js Library among Engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
 
-    const options = [
-        {
-            label: "The color Red",
-            value: 'red'
-        },
-        {
-            label:"The color Green",
-            value: "green"
-        },
-        {
-            label: "A Shade of Blue",
-            value:"blue"
-        }, 
-        {
-            label: "The color orange",
-            value: 'orange'
-        }
-    ]
+const options = [
+  {
+    label: "The color Red",
+    value: "red",
+  },
+  {
+    label: "The color Green",
+    value: "green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue",
+  },
+  {
+    label: "The color orange",
+    value: "orange",
+  },
+];
 
- const App = () => {
-    //states
-    //const [selected, setSelected] = React.useState(options[0])
-    // const [showDropdown, setShowDropdown] = React.useState(true);
+const showAccordion = () => {
+  if (
+    window.location.pathname === "/" ||
+    window.location.pathname === "/accordion"
+  ) {
+    return <Accordion items={items} />;
+  }
+};
 
-    // //methods
-    // const handleDropdown = () => {
-    //     setShowDropdown(!showDropdown)
-    // }
+const showList = () => {
+  if (window.location.pathname === "/list") {
+    return <Search />;
+  }
+};
 
-    return (//temporarily rendering Dropdown component alone
+const showDropdown = () => {
+  if (window.location.pathname === "/dropdown") {
+    return <Dropdown options={options} />;
+  }
+};
+
+const ShowTranslate = () => {
+  if (window.location.pathname === "/translate") {
+    return <Translate />;
+  }
+};
+const App = () => {
+  return (
     <div>
-            {/* <button onClick={handleDropdown}>Toggle Dropdown</button>
-        {
-            showDropdown ?
-                <Dropdown 
-                    selected={selected} 
-                    onSelectedChange ={setSelected}
-                    options ={options} 
-                /> : null
-                } */}
-                <Translate />
-        </div>
-        )
-}
+      {showAccordion()}
+      {showList()}
+      {showDropdown()}
+      {ShowTranslate()}
+    </div>
+  );
+};
 
 export default App;
